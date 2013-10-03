cecf6bbbe86b3f94a84f56206c75861a7828791e
Boris Marin <borismarin@gmail.com>
2013-10-03 15:40:20 +0100

    graphics

--- nrn/full_test.py
+++ nrn/full_test.py
@@ -49,16 +49,38 @@ def create_comp():
     return comp
 
 
+def create_graphs():
+    g_v = h.Graph()
+    g_v.size(0,5,-80,40)
+    h.graphList[0].append(g_v)
+    g_v.addexpr('v(0.5)', 1, 1, 0.8, 0.9, 2)
+
+    g_ica = h.Graph()
+    g_ica.size(0,5,-0.04,0.02)
+    h.graphList[0].append(g_ica)
+    g_ica.addexpr("ica", 1, 1, 0.49214, 0.971132, 2)
+    g_ica.addexpr("ica_nacax", 2, 1, 0.496598, 0.980711, 2)
+    g_ica.addexpr("ica_capump", 3, 1, 0.50973, 0.692392, 2)
+    g_ica.addexpr("ica_cachan", 4, 1, 2.50949, 0.00180314, 1)
+    g_ica.addexpr("ica_pmp_cadifpmp", 5, 1, 2.44857, 0.0350366, 1)
+
+    g_ca = h.Graph()
+    g_ca.size(0,5,0,0.5)
+    h.graphList[0].append(g_ca)
+    g_ca.addexpr("cai*1e4", 2, 1, 0.367087, 0.966017, 2)
+    g_ca.addexpr("ca_cadifpmp[5]*1e4", 1, 1, 0.367089, 0.876387, 2)
+    g_ca.addexpr("ca_cadifpmp[2]*1e4", 3, 1, 0.366456, 1.03701, 2)
+    
+
+
+
+
 comp = create_comp()
+
 stim = h.IClamp(0.5, sec=comp)
 stim.delay = 0
 stim.dur = 0.1
 stim.amp = 0.05
 
-
-
-g = h.Graph(0)
-g.size(0,5,-80,40)
-h.graphList[0].append(g)
-g.view(0, -80, 5, 120, 66, 52, 300.48, 200.32)
-g.addexpr("v(.5)", 1, 1, 0.8, 0.9, 2)
+create_graphs()
+h.run()
