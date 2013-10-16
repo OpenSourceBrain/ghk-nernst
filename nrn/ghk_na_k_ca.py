b009eebecefd00e76f05df6cce909fb782667d5e
Boris Marin <borismarin@gmail.com>
2013-10-16 17:19:32 +0100

    more pythonic way of talking to neuron

--- nrn/ghk_na_k_ca.py
+++ nrn/ghk_na_k_ca.py
@@ -9,8 +9,7 @@ h.celsius = 16.3
 
 def create_comp(name = 'soma'):
     
-    h('create ' + name)
-    comp = h.__getattribute__(name)
+    comp = h.Section('soma')
 
     comp.insert('HHna')
     comp.insert('HHk')
@@ -35,70 +34,39 @@ def create_comp(name = 'soma'):
     print 'phi times area to be used in lems', phi * area
     comp(0.5).cabuff.phi = phi
 
-    h('cao0_ca_ion = 2')
-    h('cai0_ca_ion = 3e-6')
-    h('nao = 115')
-    h('nai = 15')
+    h.cao0_ca_ion = 2
+    h.cai0_ca_ion = 3e-6
+    h.nao = 115
+    h.nai = 15
 
     h.ion_style("na_ion", 1, 2, 1, 0, 0) 
 
     return comp
 
-
-def create_nrn_graphs(name):
-    g_v = h.Graph()
-    g_v.size(0,5,-80,40)
-    h.graphList[0].append(g_v)
-    g_v.addexpr(name + '.v(0.5)', 1, 1, 0.8, 0.9, 2)
-
-    g_ica = h.Graph()
-    g_ica.size(0,5,-0.04,0.02)
-    h.graphList[0].append(g_ica)
-    g_ica.addexpr(name + ".ica", 1, 1, 0.49214, 0.971132, 2)
-    g_ica.addexpr(name + ".ica_cachanghk", 4, 1, 2.50949, 0.00180314, 1)
-
-    g_ca = h.Graph()
-    g_ca.size(0,5,0,0.5e-4)
-    h.graphList[0].append(g_ca)
-    g_ca.addexpr(name + ".cai", 2, 1, 0.367087, 0.966017, 2)
-
-    #g_na = h.Graph()
-    #g_na.size(0,5,0,20)
-    #h.graphList[0].append(g_na)
-    #g_na.addexpr(name + ".nai", 2, 1, 0.367087, 0.966017, 2)
     
-def plot_timeseries(varlist):
-    from pylab import plot, show, figure
-    from numpy import savetxt
+def plot_timeseries(vdict, varlist):
+    from pylab import plot, show, figure, title
 
-    v = get_dumps(varlist)
-    savetxt('/tmp/nrnghk.dat', v)
-    for i in range(1, v.shape[1]):
+    t = vdict['t']
+    for n in varlist:
         figure()
-        plot(v[:,0], v[:,i])
+        plot(t, vdict[n], label=n)
+        title(n)
+
+    
     show()
-    
 
-    
-def create_dumps(secname, varlist):
-    h('objref t_vec')
-    h('t_vec = new Vector()')
-    h('t_vec.record(&t)')
+def create_dumps(section, varlist):
+    recordings = {n: h.Vector() for n in varlist}
 
-    for vv in varlist:
-        v = vv[:vv.index('(')] + '_vec' 
-        h('objref ' + v)
-        h(v + " = new Vector()")
-        h(v + '.record(&' + secname + '.' + vv + ')')
-        
-        
-def get_dumps(varlist):
-    from numpy import c_
-    pvs = [h.t_vec.to_python()]
-    for vv in varlist:
-        v = vv[:vv.index('(')] + '_vec' 
-        pvs.append(h.__getattribute__(v).to_python())
-    return c_[pvs[:]].T
+    for (vn, v) in recordings.iteritems():
+        v.record(section(0.5).__getattribute__('_ref_' + vn))
+    
+    recordings['t'] = h.Vector()
+    recordings['t'].record(h._ref_t)
+
+    return recordings
+
 
 def run(tstop=10, dt=0.001):
     h.dt = dt
@@ -119,8 +87,10 @@ stim.dur = 0.1
 stim.amp = 0.05
 
 varlist = ['v(0.5)', 'ica(0.5)', 'cai(0.5)']
-#create_nrn_graphs(name)
-create_dumps(name, varlist)
+varlist = ['v', 'ica', 'cai']
+
+ds = create_dumps(comp, varlist)
+
 run(20, 0.001)
 
-plot_timeseries(varlist)
+plot_timeseries(ds, varlist)
