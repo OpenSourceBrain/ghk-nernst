e73a88e618b1eb8b3b958f250d8d987509a6b465
Boris Marin <borismarin@gmail.com>
2014-02-26 18:00:32 +0000

    trying to get nrn compilation right from travis

--- .travis/getnrn.sh
+++ .travis/getnrn.sh
@@ -4,6 +4,7 @@ mkdir neuron
 cd neuron
 hg clone http://www.neuron.yale.edu/hg/neuron/nrn
 cd nrn
+./build.sh
 ./configure --prefix=`pwd` --without-iv --with-nrnpython=`python -c 'import sys; print sys.executable'`
 make
 make install
