b03f010615f16e2b2b1c4ce7de2697cbe1108adb
Boris Marin <borismarin@gmail.com>
2014-02-27 15:34:49 +0000

    travis...

--- .travis/getnrn.sh
+++ .travis/getnrn.sh
@@ -8,11 +8,11 @@ cd nrn
 ./build.sh
 ./configure --prefix=`pwd` --without-iv --with-nrnpython=`python -c 'import sys; print sys.executable'`
 make
-sudo make install
+make install
 
 cd src/nrnpython
 #python setup.py install --prefix=$HOME/local
-sudo pip install -e .
+pip install -e .
 cd $HOME
 
 
